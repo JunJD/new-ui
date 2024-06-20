60018def4056c067d5f4194fe593aec997106ee1
丁俊杰 <10725046+JunJD@user.noreply.gitee.com>
2024-06-21 07:31:15 +0800

    feat: api跨域问题尝试解决

--- src/app/api/login/route.ts
+++ src/app/api/login/route.ts
@@ -1,6 +1,23 @@
 import { NextResponse } from "next/server"
- 
-export const POST = async function (req: Request) {
+import Cors from 'cors';
+// Initialize the cors middleware
+const cors = Cors({
+    origin: "*",
+    methods: ['GET', 'HEAD'],
+});
+
+function runMiddleware(req: Request, res: NextResponse, fn: any) {
+    return new Promise((resolve, reject) => {
+        fn(req, res, (result: any) => {
+            if (result instanceof Error) {
+                return reject(result);
+            }
+            return resolve(result);
+        });
+    });
+}
+export const POST = async function (req: Request, res: NextResponse) {
+    await runMiddleware(req, res, cors);
     const { usercode, password } = await req.json()
     const response = await fetch(`
     https://jgjfjdcgl.gat.zj.gov.cn:5102/inf_zpm/hz_mysql_api/BatteryBinding/login?usercode=${usercode}&password=${password}&city=0573
@@ -15,5 +32,5 @@ export const POST = async function (req: Request) {
     })
     const data = await response.json()
 
-    return NextResponse.json({ ...data  }, { status: 200 })
+    return NextResponse.json({ ...data }, { status: 200 })
 }
